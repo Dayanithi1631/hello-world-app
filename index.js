7729512e74eb9a67cde18d3b8ad54b54d139a2d1
Dayanithi Ravi <dayanithi.ravi1631@gmail.com>
2025-03-23 19:58:36 +0530

    new change

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // index.js
 const http = require('http');
 
+const PORT = 3000; // You can change this to any port you like
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Node.js on EC2!\n');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World from EC2 Node.js!\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
