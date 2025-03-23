2838cc3d767d2c3e0df54b454a5876fa3c3ccb3f
Dayanithi Ravi <dayanithi.ravi1631@gmail.com>
2025-03-23 20:18:58 +0530

    latest commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000; // You can change this to any port you like
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Dayanithi from EC2 Node.js!\n');
+  res.end('Hello world from EC2 Node.js!\n');
 });
 
 server.listen(PORT, () => {
